03d69e55410d5ff662fb625c31ddd38ca7d51a1b
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2024-05-26 23:03:05 +0200

    Add end-to-end test

---
+++ test/test_full_cli.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -eu
+set -o pipefail
+
+mkdir -p files-random
+mkdir -p files-random-original
+
+# Create 100 files of varying sizes (1-1000 bytes)
+for i in $(seq 100); do
+  size=$((1 + $RANDOM % 1000))
+  dd bs=1 count=$size if=/dev/random > "./files-random/$i" 2>/dev/null
+done
+cp -Rf ./files-random/* ./files-random-original
+
+diff -r ./files-random ./files-random-original
+
+# python cli.py build
+# python cli.py upload-all
+# python cli.py download-all
+# python cli.py extract-all
+
+export AWS_ACCESS_KEY_ID="minioadmin"
+export AWS_SECRET_ACCESS_KEY="minio123"
+export AWS_DEFAULT_REGION="eu-west-2" # Unused, but needed: https://stackoverflow.com/a/68348234/118608
+aws --endpoint-url http://127.0.0.1:9000/ s3 mb s3://bitum-bucket || true
+
+set -x
+
+# Upload files
+python bitum/cli.py upload --endpoint-url http://127.0.0.1:9000/ --bucket bitum-bucket files-random # python cli.py sync
+
+# Delete files
+/bin/rm -rf files-random/
+mkdir -p files-random/
+
+# Download files
+python bitum/cli.py download --endpoint-url http://127.0.0.1:9000/ --bucket bitum-bucket files-random # python cli.py sync
+
+# Check that the downloaded files are correct
+diff -r ./files-random ./files-random-original
+
+/bin/rm -rf files-random/ files-random-original/
